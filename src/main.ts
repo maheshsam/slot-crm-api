37c8860d3f5beb71c6ec49b81eeb74574298ea68
mahesh <maheshs@wonderwall.team>
2023-04-09 21:16:15 +0530

    Added swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser'; 
+import * as cookieParser from 'cookie-parser';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'; 
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('ESG Soft Slot CRM API Documentation')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const configService = app.get<ConfigService>(ConfigService);
   app.setGlobalPrefix(configService.get<string>('BASE_URI') || "");
   app.use(cookieParser());
