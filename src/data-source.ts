996fe2cace468cb57603686923914ad59e5d09fc
mahesh <maheshs@wonderwall.team>
2023-04-09 19:04:23 +0530

    removed db creds

--- src/data-source.ts
+++ src/data-source.ts
@@ -12,16 +12,11 @@ dotenv.config({ path: `.env.${env}` });
 
 export const appDataSource = new DataSource({
   type: 'mysql',
-  // host: process.env.DB_HOST || '127.0.0.1',
-  // port: process.env.DB_PORT || 27017,
-  // username: process.env.DB_USER,
-  // password: process.env.DB_PASSWORD,
-  // database: process.env.DB_NAME,
-  host: 'esg-soft-slot-crm-do-user-13859999-0.b.db.ondigitalocean.com',
-  port: 25060,
-  username: 'doadmin',
-  password: 'AVNS_OgsNAlpPtxBAfFlsuFa',
-  database: 'defaultdb',
+  host: process.env.DB_HOST || '127.0.0.1',
+  port: process.env.DB_PORT || 27017,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [__dirname + '/entity/*.ts'],
   migrations: [__dirname + '/migrations/*.ts'],
 } as DataSourceOptions);
